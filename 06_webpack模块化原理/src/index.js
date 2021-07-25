8697275b426f5dfc4cfe860f4442f829808c4cda
wangxuyu <wang_xuyu@uxsino.com>
2021-07-25 23:43:58 +0800

    修改

--- 06_webpack模块化原理/src/index.js
+++ 06_webpack模块化原理/src/index.js
@@ -1,11 +1,11 @@
 // es module导出内容，commonJs导入内容
-const math = require("./js/math")
+const { sum, mul } = require("./js/math")
 
 // CommonJS导出内容，es module导入内容
-import format from "./js/format"
+import { dateFormat, priceFormat } from "./js/format"
 
-console.log(math.sum(20, 30))
-console.log(math.mul(20, 30))
+console.log(sum(20, 30))
+console.log(mul(20, 30))
 
-console.log(format.dateFormat("aaa"))
-console.log(format.priceFormat("bbb"))
+console.log(dateFormat("aaa"))
+console.log(priceFormat("bbb"))
